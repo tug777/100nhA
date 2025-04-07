81330c6c8c3ab087e9ccab23cb159156906de184
Giulliana Cestari <dev.giullianacestari@gmail.com>
2025-04-07 10:51:47 -0300

    aula 8

--- main.js
+++ main.js
@@ -57,19 +57,23 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 
 }
 
-function classificaSenha() {
-    forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha > 11) {
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+    } else if (entropia > 35 && entropia < 57 ) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 5) {
+    } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
 
 
